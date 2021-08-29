9fbc59adf18de405a9982647c8f42fc18134241c
yuliya-d98 <83913169+yuliya-d98@users.noreply.github.com>
2021-08-29 17:37:13 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let config = {};
+  for (let keys of bracketsConfig) {
+      config[keys[0]] = keys[1];
+  }
+  let result = str.split("");
+  recursion(result);
+  function recursion(result) {
+      for (let key in config) {
+          for (let i = 0; i < result.length; i++) {
+              if (result[i] == key && result[i + 1] == config[key]) {
+                  result.splice(i, 2);
+                  recursion(result);
+              }
+          }
+      }
+  }
+  let solution = result.length > 0 ? false : true;
+  return solution;
+};
